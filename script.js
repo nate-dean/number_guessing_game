f28c19f76033edc84db5bdf26d3317e4eb2322e2
nate-dean <natedeann@protonmail.com>
2026-03-16 05:38:48 +1100

    Welcome Alert and Rules.

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-h3 {
-    margin: auto;
+// document.addEventListener('DOMContentLoaded', (event)=> {
+//     const userInput = prompt("Please Select a Difficulty");
+// })
+
+window.onload = function(){
+    alert("Welcome to the Number Guessing Game!\nI'm thinking of a number between 1 and 100.\nYou have 5 chances to guess the correct number")
+    console.log("Welcome to the Number Guessing Game!\nI'm thinking of a number between 1 and 100.\nYou have 5 chances to guess the correct number")
 }
