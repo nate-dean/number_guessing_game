8dcabb7af2b6fd894a2d4a699c036a466e7b1a08
nate-dean <natedeann@protonmail.com>
2026-03-23 06:15:36 +1100

    When askQuestion Func ends calls enterGuess
    enterGuess func not working

--- script.js
+++ script.js
@@ -11,11 +11,11 @@ function generateNum(){
     const MAX = 100;
     return Math.floor(Math.random() * (MAX - MIN + 1)) + MIN;
 };
-
+let computerNum = generateNum();
 
 //Select Difficulty
 
-console.log("Please select the Difficulty Level:")
+console.log("Please select the Difficulty Level: ")
 console.log("1. Easy (10 Chances)")
 console.log("2. Medium (5 Chances)")
 console.log("3. Hard (3 Chances)")
@@ -24,8 +24,6 @@ function selDif() {
 
 }
 
-//Get User Input
-
 const readline = require('readline');
 
 const rl = readline.createInterface({
@@ -33,34 +31,47 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
-
 const askQuestion = () => {
-    rl.question("Enter your Choice:", (answer) => {
+    rl.question("Enter your Choice: ", (answer) => {
         let modifiedAnswer;
         if(answer === "1"){
-            modifiedAnswer = "Great you have selected the Easy difficulty level!";
-            rl.close();
+            modifiedAnswer = "Great you have selected the Easy difficulty level! Let's Start the Game!";
+
         } else if (answer === "2"){
-            modifiedAnswer = "Great you have selected the Medium difficulty level!";
-            rl.close();
+            modifiedAnswer = "Great you have selected the Medium difficulty level! Let's Start the Game!";
+           
         } else if (answer === "3"){
-            modifiedAnswer = "Great you have selected the Hard difficulty level!";
-            rl.close();
+            modifiedAnswer = "Great you have selected the Hard difficulty level! Let's Start the Game!";
+            
         } else {
             modifiedAnswer = `${answer} is not a valid answer, please select from 1, 2 or 3.`;
             askQuestion();
         };
          console.log(modifiedAnswer);
-
+         enterGuess();
     });
     };
     askQuestion();
-    
 
 
+const enterGuess = () => {
+    rl.question("Enter your Guess: ", (userGuess) => {
+        
+        if (userGuess > computerNum){
+            userGuess = `Incorrect! The number is less than ${userGuess}`;
+            enterGuess();
+        } else if (userGuess < computerNum){
+            userGuess = `Incorrect! The number is less than ${userGuess}`;
+            enterGuess();
+        } else if (userGuess === computerNum){
+            userGuess = "Congratulations! You guessed the Correct Number in _ attempts!"
+            rl.close();
+        };
+    });
+};
 
 
-
+//needs to read console log then call the ask question function
 
 
 
